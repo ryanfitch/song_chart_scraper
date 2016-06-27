db56f866c0edc532bcc4c53fe5fc3801e91cfbf6
Ryan Fitch <ryanfitch@mac.com>
2016-06-27 10:30:23 -0700

    Add additional Spotify searching functionality

--- chart_grabber.rb
+++ chart_grabber.rb
@@ -4,14 +4,16 @@ require 'koala'
 require 'dotenv'
 require 'pry'
 Dotenv.load
-# require_relative 'Gemfile'
-require_relative 'song'
+# load 'Gemfile'
+load 'song.rb'
 
 
 songs = []
 time_stamp = Time.now
 RSpotify.authenticate("#{ENV['SPOTIFY_TOKEN']}", "#{ENV['SPOTIFY_SECRET']}")
 userID = RSpotify::User.find('eshea89')
+
+
 latest_playlist = userID.playlists.first
 counter = 1
 latest_playlist.tracks.each do |track|
@@ -21,26 +23,23 @@ latest_playlist.tracks.each do |track|
   t_id.spotify_track_id = track.id
   t_id.artist_name = track.artists.first.name
   t_id.pos = counter
+  counter += 1
   t_id.time_of_scraping = time_stamp.strftime("%A %B/%d/%Y")
   songs << t_id
 end
 
+songs.each do |song|
+  spotify_artist = RSpotify::Artist.search("#{song.artist_name}")
+  if spotify_artist.empty?
+    song.spotify_popularity = "N/A"
+    song.spotify_link = "N/A"
+  else
+    song.spotify_popularity = spotify_artist.last.popularity.to_s
+    song.spotify_link = spotify_artist.last.external_urls['spotify']
+  end
+end
 
-
-
-pry(binding)
-
-# song_data['ARTISTS'].each do |artist|
-#   spot_artists = RSpotify::Artist.search("#{artist}")
-#   if spot_artists.empty?
-#     song_data['SPOTIFY POPULARITY(s)'] << "N/A"
-#     song_data['SPOTIFY LINK'] << "N/A"
-#   else
-#     spot_artist << spot_artists.first
-#     song_data['SPOTIFY POPULARITY(s)'] << spot_artist.last.popularity.to_s
-#     song_data['SPOTIFY LINK'] << spot_artist.last.external_urls['spotify']
-#   end
-# end
+# pry(binding)
 
 
   
